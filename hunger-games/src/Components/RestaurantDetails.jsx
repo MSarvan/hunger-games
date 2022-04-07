a7e09cca17305002f057f38818ec93d2ddf24ddf
Saravanan <sarvan.m20@gmail.com>
2022-04-07 22:11:30 +0530

    sort function updated

--- hunger-games/src/Components/RestaurantDetails.jsx
+++ hunger-games/src/Components/RestaurantDetails.jsx
@@ -4,12 +4,83 @@ import "../Styles/RestaurantDetails.css";
 
 const RestaurantDetails = () => {
   const [details, setDetails] = useState([]);
+  const [toshow, setToshow] = useState([]);
+  console.log("toshow:", toshow);
 
   useEffect(() => {
     axios.get("http://localhost:2345/get-restaurants").then((res) => {
       setDetails(res.data);
+      setToshow(res.data);
     });
   }, []);
+
+  function handleRating(a) {
+    if (a == 4) {
+      let temp = toshow.filter((items) => {
+        if (items.rating >= 4) {
+          return items;
+        }
+      });
+      setToshow(temp);
+    }
+
+    if (a == 3) {
+      let temp = toshow.filter((items) => {
+        if (items.rating >= 3) {
+          return items;
+        }
+      });
+      setToshow(temp);
+    }
+
+    if (a == 2) {
+      let temp = toshow.filter((items) => {
+        if (items.rating >= 2) {
+          return items;
+        }
+      });
+      setToshow(temp);
+    }
+
+    if (a == 1) {
+        let temp = toshow.filter((items) => {
+          if (items.rating >= 1) {
+            return items;
+          }
+        });
+        setToshow(temp);
+      }
+  }
+
+  const costSorting = (val) => {
+    if (val == 0) {
+      // low to high
+      let temp = [...toshow];
+      temp.sort((a, b) => a.cost_for_two - b.cost_for_two);
+      setToshow(temp);
+      //console.log('temp:', temp);
+    }
+  }
+
+  const paymentFilter = (inp) => {
+      if(inp == "cash")
+      {
+        let temp = toshow.filter((items) => {
+             if (items.payment_method.cash) {
+                 return items;
+               }
+            });
+      }
+
+      if(inp == "card")
+      {
+        let temp = toshow.filter((items) => {
+            if (items.payment_method.card) {
+              return items;
+            }
+        });
+      }
+  }
 
   return (
     <div>
@@ -21,23 +92,23 @@ const RestaurantDetails = () => {
           <button>4</button>
         </div>
         <div className="rating-filter">
-          <button>4 and above</button>
-          <button>3 and above</button>
-          <button>2 and above</button>
-          <button>1 and above</button>
+          <button onClick={() => handleRating(4)}>4 and above</button>
+          <button onClick={() => handleRating(3)}>3 and above</button>
+          <button onClick={() => handleRating(2)}>2 and above</button>
+          <button onClick={() => handleRating(1)}>1 and above</button>
         </div>
         <div className="payment-filter">
-          <button>Cash</button>
-          <button>Card</button>
-          <button>All</button>
+          <button onClick={() => paymentFilter(cash)}>Cash</button>
+          <button onClick={() => paymentFilter(card)}>Card</button>
+          <button onClick={() => paymentFilter(all)}>All</button>
         </div>
         <div className="cost-filter">
-          <button>Cost - Low to High</button>
-          <button>Cost - High to Low</button>
+          <button onClick={() => costSorting(0)}>Cost - Low to High</button>
+          <button onClick={() => costSorting(1)}>Cost - High to Low</button>
         </div>
       </div>
 
-      {details.map((e) => {
+      {toshow.map((e) => {
         return (
           <div className="border">
             <img src={e.img} alt="" />
